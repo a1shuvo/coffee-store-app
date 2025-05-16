f70b87365aa7a834ff49ee771bfddb35671e2e4a
a1shuvo <a1shuvosaha@gmail.com>
2025-05-16 22:12:10 +0600

    Send Coffee data to the server and store in the mongodb database

--- coffee-store-client/src/components/AddCoffee.jsx
+++ coffee-store-client/src/components/AddCoffee.jsx
@@ -1,13 +1,26 @@
 const AddCoffee = () => {
   const handleAddCoffee = (e) => {
     e.preventDefault();
-    // const form = e.target;
+    const form = e.target;
+    const formData = new FormData(form);
+    const newCoffee = Object.fromEntries(formData.entries());
+
+    // Send coffee data to the db
+    fetch("http://localhost:3000/coffees", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("After adding to db", data));
   };
   return (
     <div className="p-24">
-      <div className="p-12 text-center space-y-4">
+      <div className="p-12 text-center space-y-4 bg-[#F4F3F0]">
         <h1 className="text-6xl">Add Coffee</h1>
-        <p>
+        <p className="w-3xl mx-auto">
           It is a long established fact that a reader will be distraceted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
@@ -15,71 +28,75 @@ const AddCoffee = () => {
         </p>
         <form onSubmit={handleAddCoffee}>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <fieldset className="fieldset">
               <label className="label font-bold">Name</label>
               <input
                 type="text"
                 name="name"
-                className="input w-full"
+                className="input w-full focus:outline-none"
                 placeholder="Enter Coffee Name"
               />
             </fieldset>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-              <label className="label font-bold">Chef</label>
+            <fieldset className="fieldset">
+              <label className="label font-bold">Quantity</label>
               <input
                 type="text"
-                name="chef"
-                className="input w-full"
-                placeholder="Enter Coffee Chef"
+                name="quantity"
+                className="input w-full focus:outline-none"
+                placeholder="Enter Coffee Quantity"
               />
             </fieldset>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <fieldset className="fieldset">
               <label className="label font-bold">Supplier</label>
               <input
                 type="text"
                 name="supplier"
-                className="input w-full"
+                className="input w-full focus:outline-none"
                 placeholder="Enter Coffee Supplier"
               />
             </fieldset>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <fieldset className="fieldset">
               <label className="label font-bold">Taste</label>
               <input
                 type="text"
                 name="taste"
-                className="input w-full"
+                className="input w-full focus:outline-none"
                 placeholder="Enter Coffee Taste"
               />
             </fieldset>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <fieldset className="fieldset">
               <label className="label font-bold">Category</label>
               <input
                 type="text"
                 name="category"
-                className="input w-full"
+                className="input w-full focus:outline-none"
                 placeholder="Enter Coffee Category"
               />
             </fieldset>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <fieldset className="fieldset">
               <label className="label font-bold">Details</label>
               <input
                 type="text"
                 name="details"
-                className="input w-full"
+                className="input w-full focus:outline-none"
                 placeholder="Enter Coffee Details"
               />
             </fieldset>
           </div>
-          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4 my-4">
+          <fieldset className="fieldset my-4">
             <label className="label font-bold">Photo URL</label>
             <input
               type="text"
               name="photo"
-              className="input w-full"
+              className="input w-full focus:outline-none"
               placeholder="Enter Photo URL"
             />
           </fieldset>
-          <input className="btn w-full btn-outline" type="submit" value="Add Coffee" />
+          <input
+            className="btn w-full btn-outline bg-[#D2B48C]"
+            type="submit"
+            value="Add Coffee"
+          />
         </form>
       </div>
     </div>
