234343eb5fe374f6cb2604c88f66dc14a3de38cc
a1shuvo <a1shuvosaha@gmail.com>
2025-05-16 21:20:55 +0600

    Create Add coffee from Client Side using daisyui

--- coffee-store-client/src/main.jsx
+++ coffee-store-client/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import "./App.css";
 import AddCoffee from "./components/AddCoffee";
 import Home from "./components/Home";
 import UpdateCoffee from "./components/UpdateCoffee";
-import "./index.css";
 import MainLayout from "./layouts/MainLayout";
 
 const router = createBrowserRouter([
